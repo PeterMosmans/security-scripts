246e1452c6cf07e2cd937d9dd2f656f9f8753eaf
Peter Mosmans <support@go-forward.net>
2014-01-07 12:34:30 +1000

    Added trap control Added scanning of multiple HTTP-enabled ports Added preflight check for existence of tool Improved regular expression for IP range

--- analyze_hosts.sh
+++ analyze_hosts.sh
@@ -9,16 +9,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# TODO: - preflight checks for all tools
-#       - preflight check on hostname  
-#       - trap control (session management, cancel scan/host/all hosts)
-#       - cleanup of non-function code
+# TODO: - preflight check on hostname 
+#       - add option to only list commands, don't execute them
+#       - add no-color option
 
 name=analyze_hosts
-version="0.41 (06-01-2014)"
+version="0.43 (07-01-2014)"
 
 # statuses
-declare -c CLOSED=-1
+declare -c ERROR=-1
 declare -c UNKNOWN=0
 declare -c OPEN=1
 declare -c UP=1
@@ -45,13 +44,14 @@ declare -i whois=$UNKNOWN
 declare -i hoststatus=$UNKNOWN
 declare -i loglevel=$STDOUT
 declare -i portstatus=$UNKNOWN
+declare -c WEBPORTS=80,443,8080
 
 datestring=$(date +%Y-%m-%d)
 workdir=/tmp
 
 # temporary files
 portselection=/tmp/$name-$(date +%s)-portselection.tmp
-tmpfile=/tmp/$name.tmp
+tmpfile=$(mktemp -q)
 
 # colours
 declare -c BLUE='\E[1;49;96m'
@@ -61,8 +61,11 @@ declare -c LIGHTRED='\E[2;49;31m'
 declare -c GREEN='\E[1;49;32m'
 declare -c LIGHTGREEN='\E[2;49;32m'
 
-trap cleanup INT
+trap abortscan INT
+trap cleanup EXIT QUIT
 umask 177
+
+flag=$OPEN
 
 # define functions
 prettyprint() {
@@ -95,7 +98,7 @@ usage() {
     echo " -p, --ports             nmap portscan"
     echo "     --allports          nmap portscan (all ports)"
     echo " -s                      check SSL configuration"
-    echo "     --ssl               extra check for SSL configuration"
+    echo "     --ssl               alternative check of SSL configuration"
     echo " -t                      check webserver for HTTP TRACE method"
     echo "     --trace             extra check for HTTP TRACE method"
     echo " -w, --whois             perform WHOIS lookup"
@@ -107,7 +110,7 @@ usage() {
     echo " -l, --log               log each scan in a separate logfile"
     echo " -o, --output=FILE       concatenate all results into FILE"
     echo " -q, --quiet             quiet"
-    echo " -v, --verbose           print more information on screen"
+    echo " -v, --verbose           show server responses"
     echo ""
     echo "     --version           print version information and exit"
     echo ""
@@ -122,21 +125,28 @@ usage() {
 }
 
 # setlogfilename (name)
-# sets the GLOBAL variable logfile and currentscan
+# sets the GLOBAL variable logfile and tool
 setlogfilename() {
     logfile=${target}_$1_${datestring}.txt
-    currentscan=$1
+    if type $1 >/dev/null 2>&1; then
+        tool=$1
+    else
+        prettyprint "ERROR: The program $1 could not be found - aborting test" $RED
+        tool=$ERROR
+    fi
 }
 
 purgelogs() {
     if [[ -f "$logfile" ]]; then
-        if (($loglevel&$VERBOSE)); then showstatus "$(cat $logfile)"; fi
+        if (($loglevel&$VERBOSE)) || [[ ! -z "$1" ]]; then
+            showstatus "$(cat $logfile)"
+        fi
         if (($loglevel&$RAWLOGS)); then
             grep -v '^[#%]' $logfile >> $outputfile
         fi
         if !(($loglevel&$SEPARATELOGS)); then rm $logfile 1>/dev/null 2>&1; fi
     fi
-    currentscan=
+    tool=$ERROR
 }
 
 # showstatus (message) [COLOR] [NONEWLINE]
@@ -154,6 +164,19 @@ showstatus() {
             if !(($loglevel&$QUIET)); then echo "$1"; fi
         if (($loglevel&$LOGFILE)); then echo "$1" >> $outputfile; fi
     fi
+}
+
+startup() {
+    showstatus "$name version $version starting on $(date +%d-%m-%Y' at '%R)"
+    if (($loglevel&$LOGFILE)); then
+        if [[ -n $appendfile ]]; then
+            showstatus "appending to existing file $outputfile"
+        else
+            showstatus "logging to $outputfile"
+        fi
+    fi
+    showstatus "scanparameters: $fulloptions"
+    if [[ -n "$workdir" ]]; then pushd $workdir 1>/dev/null; fi
 }
 
 version() {
@@ -174,31 +197,32 @@ version() {
 checkifportopen() {
     portstatus=$UNKNOWN
     if [[ -s "$portselection" ]]; then
-        portstatus=$CLOSED
+        portstatus=$ERROR
         grep -q " $1/open/" $portselection && portstatus=$OPEN
     fi
 }
 
 do_sslscan() {
     checkifportopen 443
-    if (($portstatus==$CLOSED)); then
+    if (($portstatus==$ERROR)); then
         showstatus "port 443 closed" $BLUE
         return
     fi
 
     setlogfilename "sslscan"
-    if (($sslscan==$BASIC)); then
-        showstatus "performing sslscan..."
-        sslscan --no-failed $target:443 > $logfile
-        grep -qe "ERROR: Could not open a connection to host $target on port 443" $logfile||portstatus=$CLOSED
-        if (($portstatus==$CLOSED)) ; then
-            showstatus "could not connect to port 443" $BLUE
-        else
-            showstatus "$(awk '/(Accepted).*(SSLv2|EXP|MD5|NULL| 40| 56)/{print $2,$3,$4,$5}' $logfile)" $RED
-        fi
-        purgelogs
+    if (($sslscan==$BASIC)) && (($tool!=$ERROR)); then
+       showstatus "performing sslscan..."
+       sslscan --no-failed $target:443 > $logfile
+       grep -qe "ERROR: Could not open a connection to host $target on port 443" $logfile||portstatus=$ERROR
+       if (($portstatus==$ERROR)) ; then
+           showstatus "could not connect to port 443" $BLUE
+       else
+           showstatus "$(awk '/(Accepted).*(SSLv2|EXP|MD5|NULL| 40| 56)/{print $2,$3,$4,$5}' $logfile)" $RED
+       fi
+       purgelogs
     fi
 
+    setlogfilename "nmap"
     if (($sslscan>=$ADVANCED)); then
         showstatus "performing nmap sslscan..."
         nmap -p 443,8080 --script ssl-enum-ciphers --open -oN $logfile $target 1>/dev/null 2>&1 </dev/null
@@ -213,43 +237,35 @@ do_sslscan() {
 
 do_fingerprint() {
     checkifportopen 80
-    if (($portstatus==$CLOSED)); then
+    if (($portstatus==$ERROR)); then
         showstatus "port 80 closed" $BLUE
     else
         setlogfilename "whatweb"
-        showstatus "performing whatweb fingerprinting..."
-        whatweb -a3 --color never $target --log-brief $logfile 1>/dev/null 2>&1
-        purgelogs
+        if (($tool!=$ERROR)); then
+            showstatus "performing whatweb fingerprinting..."
+            whatweb -a3 --color never $target --log-brief $logfile 1>/dev/null 2>&1
+            purgelogs $VERBOSE
+        fi
     fi
 }
 
 do_nikto() {
-    checkifportopen 80
-    if (($portstatus==$CLOSED)); then
-        showstatus "port 80 closed" $BLUE
-    else
-        setlogfilename "nikto"
-        showstatus "performing nikto webscan on port 80... " $NONEWLINE
+    setlogfilename "nikto"
+    if (($tool!=$ERROR)); then
         if [[ $target =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
             showstatus "FQDN preferred over IP address"
         fi
-        nikto -host $target -Format txt -output $logfile 1>/dev/null 2>&1 </dev/null
-        purgelogs
-    fi
-
-    if (($nikto&$ADVANCED)); then
-        checkifportopen 443
-        if (($portstatus==$CLOSED)); then
-            showstatus "port 443 closed" $BLUE
-        else
+        for port in ${WEBPORTS//,/ }; do
             setlogfilename "nikto"
-            showstatus "performing nikto webscan on port 443... " $NONEWLINE
-            if [[ $target =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-                showstatus "FQDN preferred over IP address"
+            checkifportopen $port
+            if (($portstatus==$ERROR)); then
+                showstatus "port $port closed" $GREEN
+            else
+                showstatus "performing nikto webscan on port $port... "
+                nikto -host $target:$port -Format txt -output $logfile 1>/dev/null 2>&1 </dev/null
             fi
-            nikto -host $target:443 -ssl -Format txt -output $logfile 1>/dev/null 2>&1 </dev/null
             purgelogs
-        fi
+        done
     fi
 }
 
@@ -277,59 +293,44 @@ do_portscan() {
 }
 
 do_trace() {
-    checkifportopen 80
-    if (($portstatus==$CLOSED)); then
-        showstatus "port 80 closed" $GREEN
-    else
-        setlogfilename "curl"
-        showstatus "trying TRACE method on port 80... " $NONEWLINE
-        curl -q --insecure -i -m 30 -X TRACE -o $logfile http://$target/ 1>/dev/null 2>&1
-        if [[ -s $logfile ]]; then
-            status=$(awk 'NR==1 {print $2}' $logfile)
-            if [[ $status -le 302 ]]; then
-                showstatus "TRACE enabled on port 80" $RED
+    setlogfilename "curl"
+    if (($tool!=$ERROR)); then
+        for port in ${WEBPORTS//,/ }; do
+            setlogfilename "curl"
+            checkifportopen $port
+            if (($portstatus==$ERROR)); then
+                showstatus "port $port closed" $GREEN
             else
-                showstatus "disabled (HTTP statuscode $status)" $GREEN
+                showstatus "trying TRACE method on port $port... " $NONEWLINE
+                curl -q --insecure -i -m 30 -X TRACE -o $logfile http://$target/ 1>/dev/null 2>&1
+                if [[ -s $logfile ]]; then
+                    status=$(awk 'NR==1 {print $2}' $logfile)
+                    if [[ $status -le 302 ]]; then
+                        showstatus "TRACE enabled on port $port" $RED
+                    else
+                        showstatus "disabled (HTTP statuscode $status)" $GREEN
+                    fi
+                else
+                    showstatus "could not connect to port $port" $GREEN
+                fi
             fi
-        else
-            showstatus "could not connect to port 80" $GREEN
-        fi
-        purgelogs
-    fi
-
-    checkifportopen 443
-    if (($portstatus==$CLOSED)); then
-        showstatus "port 443 closed" $GREEN
-    else
-        setlogfilename "curl"
-        showstatus "trying TRACE method on port 443... " $NONEWLINE
-        curl -q --insecure -i -m 30 -X TRACE -o $logfile https://$target/ 1>/dev/null 2>&1
-        if [[ -s $logfile ]]; then
-            status=$(awk 'NR==1 {print $2}' $logfile)
-            if [[ $status -eq 200 ]]; then
-                showstatus "TRACE enabled on port 443" $RED
-            else
-                showstatus "disabled (HTTP statuscode $status)" $GREEN
-            fi
-        else
-            showstatus "could not connect to port 443" $BLUE
-        fi
-        purgelogs
+            purgelogs
+        done
     fi
 
     if (($trace>=$ADVANCED)); then
-        setlogfilename "nmap-trace"
-        showstatus "trying nmap TRACE method on ports 80,443 and 8080... " $NONEWLINE
-        nmap -p80,443,8080 --open --script http-trace -oN $logfile $target 1>/dev/null 2>&1 </dev/null
+        setlogfilename "nmap"
+        showstatus "trying nmap TRACE method on ports $WEBPORTS... " $NONEWLINE
+        nmap -p$WEBPORTS --open --script http-trace -oN $logfile $target 1>/dev/null 2>&1 </dev/null
 	if [[ -s $logfile ]]; then
-        status="$(awk '{FS="/";a[++i]=$1}/TRACE is enabled/{print "TRACE enabled on port "a[NR-1]}' $logfile)"
-        if [[ -z "$status" ]]; then
-            showstatus "disabled"  $GREEN
-        else
-            showstatus "$status" $RED
+            status="$(awk '{FS="/";a[++i]=$1}/TRACE is enabled/{print "TRACE enabled on port "a[NR-1]}' $logfile)"
+            if [[ -z "$status" ]]; then
+                showstatus "disabled"  $GREEN
+            else
+                showstatus "$status" $RED
+            fi
         fi
         purgelogs
-fi
     fi
 }
 
@@ -403,13 +404,25 @@ looptargets() {
     fi
 }
 
+abortscan() {
+    flag=$ERROR
+     if (($tool!=$ERROR)); then
+         showstatus ""
+         prettyprint "$tool interrupted..." $RED
+         purgelogs
+         prettyprint "press Ctrl-C again to abort scan, or wait 10 seconds to resume" $BLUE
+         sleep 10 && flag=$OPEN
+     fi
+     if ((flag==$ERROR)); then exit 1; fi
+}
+
 cleanup() {
-    showstatus "cleaning up temporary files.."
-    if [[ -f "$logfile" ]]; then
-        showstatus "dirty shutdown during $currentscan detected"
-#        showstatus "scanstatus is saved - you can resume this scan" 
+    trap '' EXIT INT QUIT
+    if (($tool!=$ERROR)); then 
+        prettyprint "$tool interrupted..." $RED
+        purgelogs
     fi
-    purgelogs
+    showstatus "cleaning up temporary files..."
     if [[ -s "$portselection" ]]; then rm "$portselection" ; fi
     if [[ -s "$tmpfile" ]]; then rm "$tmpfile" ; fi
     if [[ -n "$workdir" ]]; then popd 1>/dev/null ; fi
@@ -497,29 +510,17 @@ if [[ ! -s "$inputfile" ]]; then
         echo "Nothing to do... no target specified"
         exit
     fi
-    if [[ $1 =~ [0-9]-[0-9] ]]; then
+    if [[ $1 =~ -.*[0-9]$ ]]; then
         nmap -nsL $1 2>/dev/null|awk '/scan report/{print $5}' >$tmpfile
         inputfile=$tmpfile
     fi
     target=$1
 fi
 
+if ! type nmap >/dev/null 2>&1; then
+    prettyprint "ERROR: the program nmap is needed but could not be found" $RED
+    exit
+fi
 
-showparameters() {
-    if (($loglevel&$LOGFILE)); then
-        if [[ -n $appendfile ]]; then
-            showstatus "appending to existing file $outputfile"
-        else
-            showstatus "logging to $outputfile"
-        fi
-    fi
-    showstatus "scanparameters: $fulloptions"
-}
-
-showstatus "$name version $version starting on $(date +%d-%m-%Y' at '%R)"
-showparameters
-
-if [[ -n "$workdir" ]]; then pushd $workdir 1>/dev/null; fi
-
+startup
 looptargets
-cleanup
